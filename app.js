cc18e8a45a9d397c5a4aba3ad2bdbd2ce3d89402
DM2006he <ducminh2006he@gmail.com>
2023-08-04 21:08:45 +0700

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+const age = prompt("write your age here")
+
+console.log(age)
+if (age >= 16) {
+    console.log("dc xem phim")
+    }
+    if(age < 16) {
+        console.log("ko dc xem phim")
+    }
